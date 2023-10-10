68202f9ed940e9b5da4de39e8098b7831810992a
mohim-uddin <mohimstudent@gmail.com>
2023-10-10 00:31:41 -0400

    hey

---
+++ web design and development/add.java
@@ -0,0 +1,15 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class addition {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+        int b = obj.nextInt();
+        int c = obj.nextInt();
+        
+        int sum = a+b+c;
+        System.out.println("total is "+sum);
+        
+    }
+}
